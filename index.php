8e466ef35306bff1e44188d5cd02df9b649b7fb1
Dirty Sauce <de.x412@gmail.com>
2013-10-07 19:46:15 -0400

    added fennb/phirehose

--- index.php
+++ index.php
@@ -2,3 +2,19 @@
 
 require_once 'vendor/autoload.php';
 
+/*
+ * abraham/twitteroauth
+
+define('CONSUMER_KEY', 'vd4d1M6fHJRjBFC2nZ4Dcg');
+define('CONSUMER_SECRET', '1m5HJ3blPp8lPuVTZ6lMOX7KE0SNecGYbWfCDRg');
+define('OAUTH_TOKEN', '167981712-RVQJw1gyhnXZpSZCuLmiTQa6mmPQJviDAyoqXLya');
+define('OAUTH_SECRET', 'vmm2bKKrxuoQWeVYbW3L4bMctq1GL2lDuueTwEveAyw');
+define('OAUTH_CALLBACK', 'http://example.com/twitteroauth/callback.php');
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET);
+$connection->host = "https://stream.twitter.com/1.1/";
+
+$content = $connection->get('twitterendpoint');
+var_dump($content);
+*/
+
