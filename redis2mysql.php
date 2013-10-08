8337f689f35ade5e8c9bd9a92a9bbaa1caa7054f
Dirty Sauce <de.x412@gmail.com>
2013-10-08 04:01:18 -0400

    redis pubsub

--- redis2mysql.php
+++ redis2mysql.php
@@ -1,22 +1,92 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$redis = new Predis\Client();
+class redis2mysql {
 
-$count = count($redis->hgetall('penspoints'));
-$tweets = array();
-if ($count > 300){
-	$raw = $redis->hgetall('penspoints');
-	foreach ($raw as $key => $data){
-		$data = preg_split("/:::/", $data);
-		$tweets[] = array(
-			'tweet_id' => $key,
-			'screen_name' => $data[0],
-			'text' => utf8_encode($data[1]),
-			'date_created' => $data[2],
-			'name' => $data[3]
+	private $dbh;
+	private $redis;
+	private $client;
+	private $pubsub;
+
+	public function __construct($dbhost, $dbname, $dbuser, $dbpass){
+		$this->dbh = new DataBaseConnection($dbhost, $dbname, $dbuser, $dbpass);
+		$this->redis = new Predis\Client();
+
+		$single_server = array(
+			'host'     => '127.0.0.1',
+			'port'     => 6379,
+			'database' => 15
 		);
+		$this->client = new Predis\Client($single_server + array('read_write_timeout' => 0));
+
+		//self::countTweets();
+		self::subscribe();
+	}
+
+	private function subscribe(){
+		$this->pubsub = $this->client->pubSub();
+		$this->pubsub->subscribe('control_channel','notifications');
+
+		foreach($this->pubsub as $message){
+			switch ($message->kind) {
+				case 'subscribe':
+					echo "subscribed to {$message->channel}\n";
+					break;
+				case 'message':
+					if ($message->channel == 'control_channel'){
+						if ($message->payload == 'quit_loop'){
+							echo "Aborting pubsub loop...\n";
+							$this->pubsub->unsubscribe();
+						} else {
+							echo "Received an unrecognized command: {$message->payload}.\n";
+						}
+					} else {
+						echo "Received the following message from {$message->channel}:\n",
+								"	{$message->payload}\n\n";
+					}
+					break;
+			}
+		}
+	}
+
+	private function countTweets(){
+		if ($this->redis->get('penspoints count') > 300){
+			self::addTweetsToDB();
+		}
+	}
+
+	private function addTweetsToDB(){
+		$raw = $this->redis->hgetall('penspoints');
+		try {
+			$query = "INSERT INTO penspoints (tweet_id, screen_name, tweet_text, tweet_date, name) VALUES (:tweet_id, :screen_name, :tweet_text, :tweet_date, :name)";
+			$this->dbh->prepare($query);
+
+			foreach ($raw as $key => $data){
+				$data = preg_split("/:::/", $data);
+				$params = array(
+					':tweet_id' => $key,
+					':screen_name' => $data[0],
+					':tweet_text' => $data[1],
+					':tweet_date' => $data[2],
+					':name' => $data[3]
+				);
+				$this->dbh->exec($query, $params);
+			}
+			$this->redis->flushdb();
+			echo "success";	
+		} catch (PDOException $e){
+			echo $e->getMessage();
+		}
+	}
+
+	public function __destruct(){
+		unset($this->pubsub);
 	}
 }
 
-var_dump($tweets);
+$dbhost = "127.0.0.1";
+$dbname = "tweets";
+$dbuser = "root";
+$dbpass = "asdf.1234!";
+
+$m = new redis2mysql($dbhost, $dbname, $dbuser, $dbpass);
