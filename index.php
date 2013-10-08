2011b9342bf66d481479947dc6f5a95250c8cd6b
Dirty Sauce <de.x412@gmail.com>
2013-10-08 02:19:38 -0400

    redis2mysql and dbc.php

--- index.php
+++ index.php
@@ -10,10 +10,6 @@ class FilterTrackConsumer extends OauthPhirehose
 
 	private $redis;
 
-	public function __construct()
-	{
-		$this->redis = new Predis\Client();
-	}
     /**
     * Enqueue each status
     *
@@ -21,6 +17,8 @@ class FilterTrackConsumer extends OauthPhirehose
     */
     public function enqueueStatus($status)
     {
+    	$this->redis = new Predis\Client();
+    	//$this->redis->set('penspoints count', 0);
         /*
         * In this simple example, we will just display to STDOUT rather than enqueue.
         * NOTE: You should NOT be processing tweets at this point in a real application, instead they should be  being enqueued and processed asyncronously from the collection process.
@@ -28,17 +26,16 @@ class FilterTrackConsumer extends OauthPhirehose
         $data = json_decode($status, true);
         if (is_array($data) && isset($data['user']['screen_name'])) {
             // send to redis here...
-            $this->redis->hset('penspoints:'.$data['id'], 'time', $data['created_at']);
-        	$this->redis->hset('penspoints:'.$data['id'], 'user', $data['user']['screen_name']);
-        	$this->redis->hset('penspoints:'.$data['id'], 'text', urldecode($data['text']));
+            $this->redis->hset('penspoints', $data['id'], $data['user']['screen_name'].':::'.urldecode($data['text']).':::'.$data['created_at'].':::'.$data['user']['name']);
+            $this->redis->incr('penspoints count');
             print $data['user']['screen_name'] . ': ' . urldecode($data['text']) . "\n";
         }
     }
 }
 
 // The OAuth credentials you received when registering your app at Twitter
-define("TWITTER_CONSUMER_KEY", "vd4d1M6fHJRjBFC2nZ4Dcg");
-define("TWITTER_CONSUMER_SECRET", "1m5HJ3blPp8lPuVTZ6lMOX7KE0SNecGYbWfCDRg");
+define("TWITTER_CONSUMER_KEY", "haEuzVBrxix2FN6FLD35g");
+define("TWITTER_CONSUMER_SECRET", "DUsre4GQgMKrdXIanNhF1XeOJjP1Bll9GkEtJotaqg");
 
 // The OAuth data for the twitter account
 define("OAUTH_TOKEN", "167981712-RVQJw1gyhnXZpSZCuLmiTQa6mmPQJviDAyoqXLya");
