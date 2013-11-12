4a9da78519342bbd961122add45d61f2fbbcfcbf
yinzer <git@davidesaias.com>
2013-11-12 02:47:04 -0500

    replacing original oauth and mysql credentials, don't worry... they've been changed in twitter/locally too

--- process.php
+++ process.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 
 $dbhost = "127.0.0.1";
 $dbname = "tweets";
-$dbuser = "root";
-$dbpass = "asdf.1234!";
+$dbuser = "dbuser";
+$dbpass = "dbpass";
 
 $process = new Process($dbhost, $dbname, $dbuser, $dbpass);
--- twithose.php
+++ twithose.php
@@ -2,12 +2,12 @@
 require_once 'vendor/autoload.php';
 
 // The OAuth credentials you received when registering your app at Twitter
-define("TWITTER_CONSUMER_KEY", "RZnKdFOJiaWPgTItYDzumQ");
-define("TWITTER_CONSUMER_SECRET", "9yqD1Il4oyztxqefxbWSb090uHeLPWlm7vXPFMaZ4");
+define("TWITTER_CONSUMER_KEY", "XXXXXXXX");
+define("TWITTER_CONSUMER_SECRET", "XXXXXXXX");
 
 // The OAuth data for the twitter account
-define("OAUTH_TOKEN", "167981712-8t3Rgo3SU2sraFrTlfNfhXLM6LFpCd715qSlDMij");
-define("OAUTH_SECRET", "5ZS9O4WIKxyuMcxS8mp7A8v8wHk9N3lB0nkM40OPes");
+define("OAUTH_TOKEN", "XXXXXXXX");
+define("OAUTH_SECRET", "XXXXXXXX");
 
 // Start streaming
 $sc = new FilterTrackConsumer(OAUTH_TOKEN, OAUTH_SECRET, Phirehose::METHOD_FILTER);
